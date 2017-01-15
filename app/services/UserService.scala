12ef211559dc627080d98e5ac105edbdafdf3ad6
AxelTLarsson <mail@axellarsson.nu>
2017-01-15 13:54:14 +0100

    Refactor UserService.authenticate (monads)

--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -3,35 +3,22 @@ package services
 import models.{UserRepository, User}
 import javax.inject._
 import scala.concurrent.Future
-import play.api.libs.json.{Json, JsValue, JsError, JsSuccess}
 import play.api.Configuration
 import play.Logger
-import scala.util.Success
+import pdi.jwt.{JwtJson, JwtAlgorithm}
 
 @Singleton
 class UserService @Inject() (userRepo: UserRepository, configuration: Configuration) {
   def authenticate(name: String, password: String): Future[Option[User]] = {
     userRepo.authenticate(name, password)
   }
- 
+
   def authenticate(token: String): Option[User] = {
-    import pdi.jwt.{JwtJson, JwtAlgorithm}
-    val key = configuration.getString("play.crypto.secret").get
-    JwtJson.decodeJson(token, key, Seq(JwtAlgorithm.HmacSHA256)) match {
-      case Success(json) => {
-        (json \ "user").validate[User] match {
-          case JsSuccess(user, _) => {
-            Logger.info("Authenticated " + user)
-            Some(user)
-          }
-          case _ => None
-        }
-      }
-      case _ => {
-        Logger.error("could not decode token")
-        None
-      }
-    }
+    for {
+      key <- configuration.getString("play.crypto.secret")
+      decoded <- JwtJson.decodeJson(token, key, Seq(JwtAlgorithm.HmacSHA256)).toOption
+      user <- (decoded \ "user").validate[User].asOpt
+    } yield user
   }
 
 }
